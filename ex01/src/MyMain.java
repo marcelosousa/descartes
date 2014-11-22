eb538b3a6fe39df006079c706172d0989e819935
Mihir Mehta <mihirthegenius@gmail.com>
2014-11-21 21:15:31 -0600

    Just cleaning up stuff.

--- ex01/src/MyMain.java
+++ ex01/src/MyMain.java
@@ -16,29 +16,45 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
-import java.util.Map;
-
-import soot.jimple.*;
-import soot.Body;
-import soot.BodyTransformer;
-import soot.G;
-import soot.PackManager;
-import soot.Transform;
-import soot.Unit;
-import soot.toolkits.graph.DirectedGraph;
-import soot.toolkits.graph.ExceptionalUnitGraph;
-import soot.util.cfgcmd.CFGToDotGraph;
-import soot.toolkits.scalar.BackwardFlowAnalysis;
-import soot.Local;
-
-import com.microsoft.z3.*;
-import com.microsoft.z3.Expr;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Map;
+
+import soot.Body;
+import soot.BodyTransformer;
+import soot.G;
+import soot.Local;
+import soot.PackManager;
+import soot.Transform;
+import soot.Unit;
+import soot.jimple.BinopExpr;
+import soot.jimple.DefinitionStmt;
+import soot.jimple.EqExpr;
+import soot.jimple.GeExpr;
+import soot.jimple.GotoStmt;
+import soot.jimple.GtExpr;
+import soot.jimple.IfStmt;
+import soot.jimple.InstanceFieldRef;
+import soot.jimple.IntConstant;
+import soot.jimple.LeExpr;
+import soot.jimple.LtExpr;
+import soot.jimple.NeExpr;
+import soot.jimple.ReturnStmt;
+import soot.toolkits.graph.DirectedGraph;
+import soot.toolkits.graph.ExceptionalUnitGraph;
+import soot.toolkits.scalar.BackwardFlowAnalysis;
+import soot.util.cfgcmd.CFGToDotGraph;
+
+import com.microsoft.z3.BoolExpr;
+import com.microsoft.z3.Context;
+import com.microsoft.z3.Expr;
+import com.microsoft.z3.FuncDecl;
+import com.microsoft.z3.IntExpr;
+import com.microsoft.z3.Solver;
+import com.microsoft.z3.Sort;
+import com.microsoft.z3.Status;
+import com.microsoft.z3.Z3Exception;
 
 public class MyMain {
 
@@ -283,7 +299,6 @@ public class MyMain {
             protected void merge(Unit arg0, AnalysisContainer arg1, AnalysisContainer arg2, AnalysisContainer arg3) {
             try {
                 if (arg0 instanceof IfStmt) {
-                    BoolExpr temp3 = null;
                     if (((IfStmt)arg0).getCondition() instanceof BinopExpr) {
                         String name1 = ((BinopExpr)((IfStmt)arg0).getCondition()).getOp1().toString();
                         IntExpr temp1 = m1.get(name1);
@@ -297,25 +312,19 @@ public class MyMain {
                             temp2 = ctx.MkIntConst(name2);
                             m1.put(name2, temp2);
                         }
-                        if (((IfStmt)arg0).getCondition() instanceof EqExpr) {
-                            temp3 = ctx.MkEq(temp1, temp2);
-                        } else if (((IfStmt)arg0).getCondition() instanceof GeExpr) {
-                            temp3 = ctx.MkGe(temp1, temp2);
-                        } else if (((IfStmt)arg0).getCondition() instanceof GtExpr) {
-                            temp3 = ctx.MkGt(temp1, temp2);
-                        } else if (((IfStmt)arg0).getCondition() instanceof LeExpr) {
-                            temp3 = ctx.MkLe(temp1, temp2);
-                        } else if (((IfStmt)arg0).getCondition() instanceof LtExpr) {
-                            temp3 = ctx.MkLt(temp1, temp2);
-                        } else if (((IfStmt)arg0).getCondition() instanceof LtExpr) {
-                            temp3 = ctx.MkLt(temp1, temp2);
-                        } else if (((IfStmt)arg0).getCondition() instanceof NeExpr) {
-                            temp3 = ctx.MkNot(ctx.MkEq(temp1, temp2));
-                        }
-                        BoolExpr temp4 = ctx.MkNot(temp3);
-                        arg3.lt0 = (BoolExpr) this.ctx.MkAnd(new BoolExpr[] {this.ctx.MkImplies(temp3, arg2.lt0), this.ctx.MkImplies(temp4, arg1.lt0)});
-                        arg3.eq0 = (BoolExpr) this.ctx.MkAnd(new BoolExpr[] {this.ctx.MkImplies(temp3, arg2.eq0), this.ctx.MkImplies(temp4, arg1.eq0)});
-                        arg3.gt0 = (BoolExpr) this.ctx.MkAnd(new BoolExpr[] {this.ctx.MkImplies(temp3, arg2.gt0), this.ctx.MkImplies(temp4, arg1.gt0)});
+                        Object temp4 = ((IfStmt)arg0).getCondition();
+                        BoolExpr temp3 =
+                            (temp4 instanceof EqExpr) ? ctx.MkEq(temp1, temp2) :
+                            (temp4 instanceof GeExpr) ? ctx.MkGe(temp1, temp2) :
+                            (temp4 instanceof GtExpr) ? ctx.MkGt(temp1, temp2) :
+                            (temp4 instanceof LeExpr) ? ctx.MkLe(temp1, temp2) :
+                            (temp4 instanceof LtExpr) ? ctx.MkLt(temp1, temp2) :
+                            (temp4 instanceof LtExpr) ? ctx.MkLt(temp1, temp2) :
+                            (temp4 instanceof NeExpr) ? ctx.MkNot(ctx.MkEq(temp1, temp2)) : null;
+                        BoolExpr temp5 = ctx.MkNot(temp3);
+                        arg3.lt0 = (BoolExpr) this.ctx.MkAnd(new BoolExpr[] {this.ctx.MkImplies(temp3, arg2.lt0), this.ctx.MkImplies(temp5, arg1.lt0)});
+                        arg3.eq0 = (BoolExpr) this.ctx.MkAnd(new BoolExpr[] {this.ctx.MkImplies(temp3, arg2.eq0), this.ctx.MkImplies(temp5, arg1.eq0)});
+                        arg3.gt0 = (BoolExpr) this.ctx.MkAnd(new BoolExpr[] {this.ctx.MkImplies(temp3, arg2.gt0), this.ctx.MkImplies(temp5, arg1.gt0)});
                     }
                 } else {
                     arg3.lt0 = this.ctx.MkAnd(new BoolExpr[] {arg1.lt0, arg2.lt0});
