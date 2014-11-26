ff1503e3eb1c97e924b9aec4f32cf1385be414b0
Mihir Mehta <mihirthegenius@gmail.com>
2014-11-26 10:46:53 -0600

    Fixed test01 and added another test case.

--- ex01/test/src/test03.java
+++ ex01/test/src/test03.java
@@ -1,13 +1,13 @@
-public class test01 {
+public class test03 {
     public static void main(String[] args) {
-        test01 f = new test01();
+        test03 f = new test03();
         int a = 7;
         int b = 14;
         int x = (f.bar(21) + a) * b;
     }
     public int bar(int n) { return n + 42; }
-    int x; int y;
-    public int compare(test01 o1, test01 o2) {
+    int x; int y; int z;
+    public int compare(test03 o1, test03 o2) {
         if (o1.x < o2.x)
             return -1;
         else if (o1.x > o2.x)
@@ -15,6 +15,10 @@ public class test01 {
         else if (o1.y < o2.y)
             return -1;
         else if (o1.y > o2.y)
+            return 1;
+        else if (o1.z < o2.z)
+            return -1;
+        else if (o1.z > o2.z)
             return 1;
         else
             return 0;
