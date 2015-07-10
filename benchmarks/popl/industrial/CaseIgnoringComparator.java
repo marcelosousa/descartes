a8be7d55c8715f87dfc4c91536d03ea874278ad1
marcelosousa <dipython@gmail.com>
2015-07-10 08:59:15 -0400

    working on industrial benchmarks

--- benchmarks/popl/industrial/CaseIgnoringComparator.java
+++ benchmarks/popl/industrial/CaseIgnoringComparator.java
@@ -20,20 +20,24 @@ import java.io.Serializable;
 import java.util.Comparator;
 
 final class CaseIgnoringComparator implements Comparator<CharSequence>, Serializable {
-
+    /*
     private static final long serialVersionUID = 4582133183775373862L;
 
     static final CaseIgnoringComparator INSTANCE = new CaseIgnoringComparator();
 
     private CaseIgnoringComparator() {
     }
-
+    */
+    int length();
+    char charAt(int i);
+    
     @Override
     public int compare(CharSequence o1, CharSequence o2) {
         int o1Length = o1.length();
         int o2Length = o2.length();
-        int min = Math.min(o1Length, o2Length);
-        for (int i = 0; i < min; i++) {
+        int min = o1Length < o2Length ? o1Length : o2Length; 
+        int i = 0;
+        while (i < min) {
             char c1 = o1.charAt(i);
             char c2 = o2.charAt(i);
             if (c1 != c2) {
@@ -47,6 +51,7 @@ final class CaseIgnoringComparator implements Comparator<CharSequence>, Serializ
                     }
                 }
             }
+            i++;
         }
         return o1Length - o2Length;
     }
