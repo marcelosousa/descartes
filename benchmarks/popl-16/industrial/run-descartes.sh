60816d2528b9b8ed9769d5f69bfbd226c9189406
marcelosousa <dipython@gmail.com>
2015-07-10 16:54:34 -0400

    log files for industrial experiment

--- benchmarks/popl-16/industrial/run-descartes.sh
+++ benchmarks/popl-16/industrial/run-descartes.sh
@@ -6,8 +6,8 @@ echo "Property 1"
 for f in *.java
 do
 	echo "Verifying $f"
-        echo "Property 1" &> log/$f-p1.log
-	{ echo "descartes -p=1 -m=0: "; time descartes -p=1 -m=0 $f; } &>> log/$f-p1.log
+        echo "Property 1" &> log/$f.log
+	{ echo "descartes -p=1 -m=0: "; time descartes -p=1 -m=0 $f; } &>> log/$f.log
 #	{ echo "descartes -p=1 -m=1: "; time descartes -p=1 -m=1 $f; } &>> log/$f-p1.log
 #	{ echo "descartes -p=1 -m=2: "; time descartes -p=1 -m=2 $f; } &>> log/$f-p1.log
 done
@@ -16,8 +16,8 @@ echo "Property 2"
 for f in *.java
 do
 	echo "Verifying $f"
-        echo "Property 2" &> log/$f-p2.log
-	{ echo "descartes -p=2 -m=0: "; time descartes -p=2 -m=0 $f; } &>> log/$f-p2.log
+        echo "Property 2" &>> log/$f.log
+	{ echo "descartes -p=2 -m=0: "; time descartes -p=2 -m=0 $f; } &>> log/$f.log
 #	{ echo "descartes -p=2 -m=1: "; time descartes -p=2 -m=1 $f; } &>> log/$f-p2.log
 #	{ echo "descartes -p=2 -m=2: "; time descartes -p=2 -m=2 $f; } &>> log/$f-p2.log
 done
@@ -26,8 +26,8 @@ echo "Property 3"
 for f in *.java
 do
 	echo "Verifying $f"
-        echo "Property 3" &> log/$f-p3.log
-	{ echo "descartes -p=3 -m=0: "; time descartes -p=3 -m=0 $f; } &>> log/$f-p3.log
+        echo "Property 3" &>> log/$f.log
+	{ echo "descartes -p=3 -m=0: "; time descartes -p=3 -m=0 $f; } &>> log/$f.log
 #	{ echo "descartes -p=3 -m=1: "; time descartes -p=3 -m=1 $f; } &>> log/$f-p3.log
 #	{ echo "descartes -p=3 -m=2: "; time descartes -p=3 -m=2 $f; } &>> log/$f-p3.log
 done
