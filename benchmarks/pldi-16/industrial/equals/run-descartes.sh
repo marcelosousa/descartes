f16289e012b2b803e9adff41dcf4acef201b5d23
marcelosousa <dipython@gmail.com>
2015-11-11 11:42:34 -0500

    equals experiments

--- benchmarks/pldi-16/industrial/equals/run-descartes.sh
+++ benchmarks/pldi-16/industrial/equals/run-descartes.sh
@@ -7,10 +7,10 @@ for f in *.java
 do
 	echo "Verifying $f"
   echo "Property 4 (Transitivity)" &> log/$f-p4.log
-	{ echo "descartes -p=4 -m=0: "; time descartes -p=4 -m=0 $f; } &>> log/$f-p4.log
-#	{ echo "descartes -p=4 -m=1: "; time descartes -p=4 -m=1 $f; } &>> log/$f-p4.log
-#	{ echo "descartes -p=4 -m=2: "; time descartes -p=4 -m=2 $f; } &>> log/$f-p4.log
-#	{ echo "descartes -p=4 -m=3: "; time descartes -p=4 -m=3 $f; } &>> log/$f-p4.log
+	{ echo "descartes -p=4 -m=0: "; timeout 900 time descartes -p=4 -m=0 $f; } &>> log/$f-p4.log
+	{ echo "descartes -p=4 -m=1: "; timeout 900 time descartes -p=4 -m=1 $f; } &>> log/$f-p4.log
+	{ echo "descartes -p=4 -m=2: "; timeout 900 time descartes -p=4 -m=2 $f; } &>> log/$f-p4.log
+	{ echo "descartes -p=4 -m=3: "; timeout 900 time descartes -p=4 -m=3 $f; } &>> log/$f-p4.log
 done
 
 echo "Property 5 (Symmetry)"
@@ -18,10 +18,10 @@ for f in *.java
 do
 	echo "Verifying $f"
   echo "Property 5 (Symmetry)" &> log/$f-p5.log
-	{ echo "descartes -p=5 -m=0: "; time descartes -p=5 -m=0 $f; } &>> log/$f-p5.log
-#	{ echo "descartes -p=5 -m=1: "; time descartes -p=5 -m=1 $f; } &>> log/$f-p5.log
-#	{ echo "descartes -p=5 -m=2: "; time descartes -p=5 -m=2 $f; } &>> log/$f-p5.log
-#	{ echo "descartes -p=5 -m=3: "; time descartes -p=5 -m=3 $f; } &>> log/$f-p5.log
+	{ echo "descartes -p=5 -m=0: "; timeout 900 time descartes -p=5 -m=0 $f; } &>> log/$f-p5.log
+	{ echo "descartes -p=5 -m=1: "; timeout 900 time descartes -p=5 -m=1 $f; } &>> log/$f-p5.log
+	{ echo "descartes -p=5 -m=2: "; timeout 900 time descartes -p=5 -m=2 $f; } &>> log/$f-p5.log
+	{ echo "descartes -p=5 -m=3: "; timeout 900 time descartes -p=5 -m=3 $f; } &>> log/$f-p5.log
 done
 
 echo "Property 6 (Consistency)"
@@ -29,8 +29,8 @@ for f in *.java
 do
 	echo "Verifying $f"
   echo "Property 6 (Consistency)" &> log/$f-p6.log
-	{ echo "descartes -p=6 -m=0: "; time descartes -p=6 -m=0 $f; } &>> log/$f-p6.log
-#	{ echo "descartes -p=6 -m=1: "; time descartes -p=6 -m=1 $f; } &>> log/$f-p6.log
-#	{ echo "descartes -p=6 -m=2: "; time descartes -p=6 -m=2 $f; } &>> log/$f-p6.log
-#	{ echo "descartes -p=6 -m=3: "; time descartes -p=6 -m=3 $f; } &>> log/$f-p6.log
+	{ echo "descartes -p=6 -m=0: "; timeout 900 time descartes -p=6 -m=0 $f; } &>> log/$f-p6.log
+	{ echo "descartes -p=6 -m=1: "; timeout 900 time descartes -p=6 -m=1 $f; } &>> log/$f-p6.log
+	{ echo "descartes -p=6 -m=2: "; timeout 900 time descartes -p=6 -m=2 $f; } &>> log/$f-p6.log
+	{ echo "descartes -p=6 -m=3: "; timeout 900 time descartes -p=6 -m=3 $f; } &>> log/$f-p6.log
 done
